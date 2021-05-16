78e99f15138a8870c5b70287ca46c10dd359fc0e
AndyLeezard <77301332+AndyLeezard@users.noreply.github.com>
2021-05-16 20:23:41 +0200

    Update app_test.go

--- app/app_test.go
+++ app/app_test.go
@@ -20,7 +20,7 @@ func TestIndexPathHandler(t *testing.T) {
 
 	assert.Equal(http.StatusOK, res.Code)
 	data, _ := ioutil.ReadAll(res.Body)
-	assert.Equal("Hello, World", string(data))
+	assert.Equal("Hello World", string(data))
 }
 func TestBarPathHandler_WithoutName(t *testing.T) {
 	assert := assert.New(t)
@@ -33,19 +33,19 @@ func TestBarPathHandler_WithoutName(t *testing.T) {
 
 	assert.Equal(http.StatusOK, res.Code)
 	data, _ := ioutil.ReadAll(res.Body)
-	assert.Equal("Hello World!", string(data))
+	assert.Equal("Hello World", string(data))
 }
 
 func TestBarPathHandler_WithName(t *testing.T) {
 	assert := assert.New(t)
 
 	res := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/bar?name=tucker", nil)
+	req := httptest.NewRequest("GET", "/bar?name=Neo", nil)
 
 	mux := NewHttpHandler()
 	mux.ServeHTTP(res, req)
 
 	assert.Equal(http.StatusOK, res.Code)
 	data, _ := ioutil.ReadAll(res.Body)
-	assert.Equal("Hello tucker!", string(data))
+	assert.Equal("Hello Neo", string(data))
 }
