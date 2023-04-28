6f9e7a6f04a5294f12226f6d1f92c97d1bb0809d
Andy Lee <a.lee.intl@gmail.com>
2023-04-28 22:14:40 +0200

    use_http_error_and_newencoder

--- app/app.go
+++ app/app.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// User represents a user with basic information.
 type User struct {
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
@@ -24,16 +25,21 @@ func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	user := new(User)
 	err := json.NewDecoder(r.Body).Decode(user)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Bad Request: ", err)
+		http.Error(w, "Bad Request: "+err.Error(), http.StatusBadRequest)
 		return
 	}
+	defer r.Body.Close()
+
+	if user.FirstName == "" || user.LastName == "" || user.Email == "" {
+		http.Error(w, "Bad Request: required fields are empty", http.StatusBadRequest)
+		return
+	}
+
 	user.CreatedAt = time.Now()
 
-	data, _ := json.Marshal(user)
-	w.Header().Add("content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprint(w, string(data))
+	json.NewEncoder(w).Encode(user)
 }
 
 func barHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,7 +50,8 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s!", name)
 }
 
-func NewHttpHandler() http.Handler {
+// HttpHandler returns a new HTTP handler for the web application.
+func HttpHandler() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexHandler)
 
