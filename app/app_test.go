cffb77af9ae35572def4dbee699c90519b95c473
Andy Lee <a.lee.intl@gmail.com>
2023-04-28 22:21:13 +0200

    using_require

--- app/app_test.go
+++ app/app_test.go
@@ -1,17 +1,17 @@
-package myapp
+package main
 
 import (
-	"io/ioutil"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"
-	"encoding/json"
 	"strings"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestIndexPathHandler(t *testing.T) {
+func TestIndexHandler(t *testing.T) {
 	assert := assert.New(t)
 
 	res := httptest.NewRecorder()
@@ -21,64 +21,67 @@ func TestIndexPathHandler(t *testing.T) {
 	mux.ServeHTTP(res, req)
 
 	assert.Equal(http.StatusOK, res.Code)
-	data, _ := ioutil.ReadAll(res.Body)
-	assert.Equal("Hello World", string(data))
-}
-func TestBarPathHandler_WithoutName(t *testing.T) {
-	assert := assert.New(t)
-
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/bar", nil)
-
-	mux := NewHttpHandler()
-	mux.ServeHTTP(res, req)
-
-	assert.Equal(http.StatusOK, res.Code)
-	data, _ := ioutil.ReadAll(res.Body)
-	assert.Equal("Hello World!", string(data))
+	assert.Equal("Hello World", res.Body.String())
 }
 
-func TestBarPathHandler_WithName(t *testing.T) {
-	assert := assert.New(t)
+func TestBarHandler(t *testing.T) {
+	t.Run("WithoutName", func(t *testing.T) {
+		assert := assert.New(t)
 
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/bar?name=Neo", nil)
+		res := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/bar", nil)
 
-	mux := NewHttpHandler()
-	mux.ServeHTTP(res, req)
+		mux := NewHttpHandler()
+		mux.ServeHTTP(res, req)
 
-	assert.Equal(http.StatusOK, res.Code)
-	data, _ := ioutil.ReadAll(res.Body)
-	assert.Equal("Hello Neo!", string(data))
+		assert.Equal(http.StatusOK, res.Code)
+		assert.Equal("Hello World!", res.Body.String())
+	})
+
+	t.Run("WithName", func(t *testing.T) {
+		assert := assert.New(t)
+
+		res := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/bar?name=Neo", nil)
+
+		mux := NewHttpHandler()
+		mux.ServeHTTP(res, req)
+
+		assert.Equal(http.StatusOK, res.Code)
+		assert.Equal("Hello Neo!", res.Body.String())
+	})
 }
 
-func TestFooHandler_WithoutJson(t *testing.T) {
-	assert := assert.New(t)
+func TestFooHandler(t *testing.T) {
+	t.Run("WithoutJson", func(t *testing.T) {
+		assert := assert.New(t)
 
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/foo", nil)
+		res := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/foo", nil)
 
-	mux := NewHttpHandler()
-	mux.ServeHTTP(res, req)
+		mux := NewHttpHandler()
+		mux.ServeHTTP(res, req)
 
-	assert.Equal(http.StatusBadRequest, res.Code)
-}
+		assert.Equal(http.StatusBadRequest, res.Code)
+	})
 
-func TestFooHandler_WithJson(t *testing.T) {
-	assert := assert.New(t)
+	t.Run("WithJson", func(t *testing.T) {
+		assert := assert.New(t)
+		require := require.New(t)
 
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("POST", "/foo",
-		strings.NewReader(`{"first_name":"Neo", "last_name":"Lee", "email":"Neo@Neonet.com"}`))
+		res := httptest.NewRecorder()
+		req := httptest.NewRequest("POST", "/foo",
+			strings.NewReader(`{"first_name":"Neo", "last_name":"Lee", "email":"Neo@Neonet.com"}`))
 
-	mux := NewHttpHandler()
-	mux.ServeHTTP(res, req)
+		mux := NewHttpHandler()
+		mux.ServeHTTP(res, req)
 
-	assert.Equal(http.StatusCreated, res.Code)
+		assert.Equal(http.StatusCreated, res.Code)
 
-	user := new(User)
-	err := json.NewDecoder(res.Body).Decode(user)
-	assert.Nil(err)
-	assert.Equal("Neo", user.FirstName)
-	assert.Equal("Lee", user.LastName)
+		user := new(User)
+		err := json.NewDecoder(res.Body).Decode(user)
+		require.Nil(err)
+		assert.Equal("Neo", user.FirstName)
+		assert.Equal("Lee", user.LastName)
+	})
 }
